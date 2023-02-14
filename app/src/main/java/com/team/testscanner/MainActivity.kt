85c9a002cda9a8aa1f43f9501f0cab64391a6cdf
mukulasija <lit2020010@iiitl.ac.in>
2023-02-14 18:32:50 +0530

    question_activity

--- app/src/main/java/com/team/testscanner/MainActivity.kt
+++ app/src/main/java/com/team/testscanner/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.team.testscanner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.text_view)
+        tv.setOnClickListener {
+            val intent = Intent(this,question::class.java)
+            startActivity(intent)
+        }
     }
+
 }
