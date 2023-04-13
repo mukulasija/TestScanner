8e45f5100dcf7d970bc36b1bb87b12c7c6b3e304
mukulasija <lit2020010@iiitl.ac.in>
2023-04-13 19:01:05 +0530

    updates

--- app/src/main/java/com/team/testscanner/ui/MainActivity.kt
+++ app/src/main/java/com/team/testscanner/ui/MainActivity.kt
@@ -3,20 +3,26 @@ package com.team.testscanner.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.team.testscanner.R
+import com.team.testscanner.ui.fragments.HomeFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mNavController: NavController
+    private lateinit var navView: BottomNavigationView
+    private lateinit var bottomAppBar: BottomAppBar
+    private var isShowBottomNav = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val tv = findViewById<TextView>(R.id.text_view)
-        val intent = Intent(this, question::class.java)
-        startActivity(intent)
-        tv.setOnClickListener {
-            val intent = Intent(this, question::class.java)
-            startActivity(intent)
-        }
-    }
+        navView = findViewById(R.id.bottomNavigationView)
 
+    }
 }
