e5b657cf47cbb6c1e6653874954919b26a26a303
deepanshu731 <deepanshugera123@gmail.com>
2023-04-17 23:31:15 +0530

    added files

--- app/src/main/java/com/team/testscanner/ui/MainActivity.kt
+++ app/src/main/java/com/team/testscanner/ui/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.team.testscanner.ui
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.R.attr.button
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import androidx.fragment.app.FragmentManager
+import android.widget.Button
+import android.widget.PopupMenu
+import android.widget.Toast
+//import android.support.design.widget.FloatingActionButton
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.team.testscanner.R
-import com.team.testscanner.ui.fragments.HomeFragment
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mNavController: NavController
@@ -22,7 +25,99 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        navView = findViewById(R.id.bottomNavigationView)
+      //  navView = findViewById(R.id.bottomNavigationView)
+        val homeFragment = MyHomeFragment()
+        val homeTransaction = supportFragmentManager.beginTransaction()
+        homeTransaction.replace(R.id.my_fragment, homeFragment)
+        homeTransaction.commit()
+
+        val fabButton:FloatingActionButton=findViewById(R.id.fab)
+
+        val bottomNavigationView:BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // Attach a listener to the bottom navigation view
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.firstFragment -> {
+                    // Switch to the Home fragment
+                    switchFragment(MyHomeFragment())
+                    true
+                }
+                R.id.secondFragment -> {
+                    // Switch to the Profile fragment
+                    switchFragment(MyResultsFragment())
+                    true
+                }
+                R.id.thirdFragment -> {
+                    // Switch to the Settings fragment
+                    switchFragment(MyAnalyticsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        fabButton.setOnClickListener {
+            // Create a new instance of the fragment
+            val fragment = MyFragment()
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.my_fragment, fragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+    }
+
+    private fun switchFragment(fragment: Fragment) {
+        // Start a new transaction
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Replace the current fragment with the new fragment
+        transaction.replace(R.id.my_fragment, fragment)
+
+        // Add the transaction to the back stack
+        transaction.addToBackStack(null)
+
+        // Commit the transaction
+        transaction.commit()
+    }
+}
+class MyFragment : Fragment(R.layout.fragment_create_test_intro) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
     }
 }
+
+class MyResultsFragment : Fragment(R.layout.fragment_results) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
+class MyAnalyticsFragment : Fragment(R.layout.fragment_analytics) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
+class MyHomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
+
+//class MyHomeFragment : Fragment(R.layout.fragment_home) {
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//    }
+//}
--- app/src/main/java/com/team/testscanner/ui/fragments/AnalyticsFragment.kt
+++ app/src/main/java/com/team/testscanner/ui/fragments/AnalyticsFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [ReportFragment.newInstance] factory method to
+ * Use the [AnalyticsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ReportFragment : Fragment() {
+class AnalyticsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class ReportFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        return inflater.inflate(R.layout.fragment_analytics, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class ReportFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment ReportFragment.
+         * @return A new instance of fragment AnalyticsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            ReportFragment().apply {
+            AnalyticsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
