e050f46641aa9c8360c5a5d27bed571e1f717f8f
mukulasija <lit2020010@iiitl.ac.in>
2023-04-22 00:20:23 +0530

    test screen glide done

--- app/src/main/java/com/team/testscanner/ui/activities/TestScreen.kt
+++ app/src/main/java/com/team/testscanner/ui/activities/TestScreen.kt
@@ -3,6 +3,7 @@ package com.team.testscanner.ui.activities
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,9 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.team.testscanner.R
 import com.team.testscanner.adapters.OptionAdapter
 import com.team.testscanner.models.OptionSelector
@@ -32,17 +36,18 @@ class TestScreen : AppCompatActivity() {
          btnNext = findViewById<Button>(R.id.btnNext)
          btnSubmit = findViewById<Button>(R.id.btnSubmit)
         bindViews()
+        setUpEventListener()
     }
     private fun setUpEventListener() {
 
         btnPrevious.setOnClickListener {
             index--
-//            bindViews()
+            bindViews()
         }
 
         btnNext.setOnClickListener {
             index++
-//            bindViews()
+            bindViews()
         }
 
 //        btnSubmit.setOnClickListener {
@@ -55,7 +60,7 @@ class TestScreen : AppCompatActivity() {
 //        }
     }
     private fun bindViews() {
-
+        //yet to implement firebase database and delete this dummydatafunction
         addDummyData()
         btnPrevious.visibility = View.GONE
         btnSubmit.visibility = View.GONE
@@ -76,7 +81,7 @@ class TestScreen : AppCompatActivity() {
         val question = questions!!["question$index"]
         question?.let {
             var optionSelector = OptionSelector()
-//            setImageWithData(it.imageUrl,it.ytop,it.yend);
+            setImageWithData(it.imageUrl,it.ytop,it.yend);
             val optionAdapter = OptionAdapter(this, optionSelector)
             optionList.layoutManager = LinearLayoutManager(this)
             optionList.adapter = optionAdapter
@@ -85,14 +90,49 @@ class TestScreen : AppCompatActivity() {
     }
 
     private fun addDummyData() {
-        questions = mutableMapOf("question1" to Question("https://drive.google.com/file/d/140erkr0zjU_Y52GUpxeCcqmkwa_Bx7Qt/view?usp=share_link",300,317,"",""))
+        questions = mutableMapOf("question1" to Question("https://www.researchgate.net/publication/255640421/figure/fig1/AS:392587958603776@1470611671200/Sample-image-and-its-feature-extraction-results-Left-Original-image-right-segmented.png",300,317,"",""))
+        questions!!.put("question2", questions!!["question1"]!!)
+        val question3 =Question("https://drive.google.com/file/d/140erkr0zjU_Y52GUpxeCcqmkwa_Bx7Qt/view?usp=share_link",300,317,"","")
+        questions!!.put("question3",question3)
+        questions = mutableMapOf("question1" to question3)
     }
 
     private fun setImageWithData(imageUrl: String, ytop: Int, yend: Int) {
-        val bitmap = BitmapFactory.decodeStream(URL(imageUrl).openStream())
-        val croppedBitmap = Bitmap.createBitmap(bitmap, 0, ytop, bitmap.width, yend)
-        val croppedImageView = findViewById<ImageView>(R.id.question_Image)
-        croppedImageView.setImageBitmap(croppedBitmap)
+//        val imageView = findViewById<ImageView>(R.id.question_Image)
+//        Glide.with(this)
+//            .load(imageUrl)
+//            .into(imageView)
+//
+//        return
+
+//
+        // Load the image using Glide
+        Glide.with(this)
+            .asBitmap()
+            .load(imageUrl)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onResourceReady(
+                    resource: Bitmap,
+                    transition: Transition<in Bitmap>?
+                ) {
+                    // Get the cropped portion of the image
+                    val croppedBitmap = Bitmap.createBitmap(
+                        resource,
+                        0,          // x-coordinate of the top-left corner of the cropped area
+                        ytop,       // y-coordinate of the top-left corner of the cropped area
+                        resource.width, // width of the cropped area
+                        yend - ytop // height of the cropped area
+                    )
+
+                    // Set the cropped bitmap to the ImageView
+                    val croppedImageView = findViewById<ImageView>(R.id.question_Image)
+                    croppedImageView.setImageBitmap(croppedBitmap)
+                }
+
+                override fun onLoadCleared(placeholder: Drawable?) {
+                    // Not implemented
+                }
+            })
     }
 
 
